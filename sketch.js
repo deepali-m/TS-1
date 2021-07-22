afb790223e05549ad49817e6088f637f116fc2bc
shubhamwhj <75826441+shubhamwhj@users.noreply.github.com>
2021-07-22 12:09:02 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,8 +3,7 @@ var seaImg,shipImg;
 
 function preload(){
   seaImg = loadImage("sea.png");
-  shipImg1 = loadAnimation("ship-1.png","ship-1.png",
-                            "ship-2.png","ship-1.png");
+  shipImg1 = loadImage("ship-1.png");
 }
 
 function setup(){
@@ -14,25 +13,17 @@ function setup(){
   // Moving background
   sea=createSprite(400,200);
   sea.addImage(seaImg);
-  sea.velocityX = -5;
   sea.scale=0.3;
 
   
   ship = createSprite(130,200,30,30);
-  ship.addAnimation("movingShip",shipImg1);
+  ship.addImage("movingShip",shipImg1);
   ship.scale =0.25;
   
 }
 
 function draw() {
   background(0);
-  sea.velocityX = -3;
-
   
-  //code to reset the background
-  if(sea.x < 0){
-    sea.x = sea.width/8;
-  }
-    
   drawSprites();
 }
